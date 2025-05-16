af558f90645e843f974a331e623fd975502c0e33
ChrisCoolman <159795512+ChrisCoolman@users.noreply.github.com>
2025-05-16 09:07:29 -0400

    Add files via upload

--- discounts.cpp
+++ discounts.cpp
@@ -1,24 +1,25 @@
 #include "discounts.h"
 #include <iostream>
 
+// Store coupons in an array
 std::string validCoupons[3] = {"STUDENT", "GRANDNATIONAL", "DROP50"};
 
 // 30% student discount
 void studentDiscount(std::string coupon, int* price) {
     if(coupon == validCoupons[0]) {
-        *price *= 0.30;
+        *price -= (*price * 0.30);
     }
 }
 // 10% discount
 void grandNationalCoupon(std::string coupon, int* price) {
     if(coupon == validCoupons[1]) {
-        *price *= 0.10;
+        *price -= (*price * 0.10);
     }
 }
 
 // 50% discount
 void drop50Coupon(std::string coupon, int* price) {
     if(coupon == validCoupons[2]) {
-        *price *= 0.50;
+        *price -= (*price * 0.50);
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+// Chris Coleman
+// pgLang lab
+// 5/16/2025
+// Extra: Waits for user input at the end before closing
 #include <iostream>
 #include <string>
 #include "communications.h"
@@ -6,30 +10,46 @@
 using namespace std;
 
 int main() {
+    // Ask user for name
     cout << "What is your name" << endl;
     string name;
     getline(cin, name);
     greeting(name);
+
+    // Ask user for product name
     cout << "Please enter the name of the product" << endl;
     string product;
     getline(cin, product);
+
+    // Ask user for the price of the product
     cout << "What is the price" << endl;
     int price;
     cin >> price;
     int* pricePtr = &price;
+
+    // Ask user for the coupon they want to use
     cout << "Enter a coupon code: " << endl;
     string couponName;
     cin >> couponName;
+    // Check user's coupon code against the three we support
+    // and pass the price as a point so it can be changed by the coupon
     studentDiscount(couponName, pricePtr);
     grandNationalCoupon(couponName, pricePtr);
     drop50Coupon(couponName, pricePtr);
+    // Print the final price
     cout << "Final Price: " << price << endl;
+
+    // ask user for their address
     cout << "Where shall we deliver this product? " << endl;
     string address;
     cin.ignore();
     getline(cin, address);
     formatAdress(address);
     cout << "You live at " << address << endl;
+
+    // Print the closing message and wait for user input before closing
     cout << closing(name, product) << endl;
+    string input;
+    cin >> input;
     return 0;
 }
